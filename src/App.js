a5e6ceac8bf6fe088ccbf16f1a4679407eb48d3b
JenniferOji <g00413791@atu.ie>
2024-10-04 08:44:50 +0000

    exercise 2 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+//business logic 
+
 function App() {
   return (
+    //every visusual piece is put in here 
+    //putting in two curly brackets means wiritng in java script
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello World!</h1>
+       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
 }
