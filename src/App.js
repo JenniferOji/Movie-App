016ccda61383986162a886011122cee92e1797e8
JenniferOji <g00413791@atu.ie>
2024-10-04 09:32:26 +0000

    exercise 5 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/Navbar';
 import Header from './components/Header';
-import Content from './components/Content';
 import Footer from './components/Footer';
+import Content from './components/Content';
 
+//the footer tag will always show since it outside the route tags 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Header />} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+      
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+//What the navbar will look like when called 
+//changing the bg colour to primary makes the navbar blue
+//the "/" directs you to the page 
+const NavigationBar = () => {
+  return (
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/Navbar">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/Home">Home</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationBar;
