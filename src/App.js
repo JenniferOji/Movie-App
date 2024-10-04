5fb4d9c8b8c43cf4f8abe47c305a6448d6573836
JenniferOji <g00413791@atu.ie>
2024-10-04 09:38:52 +0000

    exercise 6 complete

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Footer from './components/Footer';
 import Content from './components/Content';
 
 //the footer tag will always show since it outside the route tags 
+//using "/" to direct to the desired change 
 function App() {
   return (
     <Router>
       <NavigationBar />
       <Routes>
         <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<Header />} />
-        <Route path="/create" element={<h1>Create Component</h1>} />
+        <Route path="/read" element={<Footer />} />
+        <Route path="/create" element={<Header />} />
       </Routes>
       
-      <Footer />
     </Router>
   );
 }
