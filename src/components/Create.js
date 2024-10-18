e69db8b32cc8b6fc532f0205802e13c781eb019a
JenniferOji <g00413791@atu.ie>
2024-10-18 09:11:44 +0000

    Lab 4 part 2

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,35 @@
-//When the component is called it will be displayed on screen 
-const Create = () => {
-    return (
-        <div>
-           <h1>Hello from the Create component</h1>
+import { useState } from "react";
+
+//creating a form that will send data to the create
+function Create() {
+  //created a state variavle and a method to update it
+  //the initial value of title will be an empty string
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            //styling from bootstrap
+            className="form-control"
+            // value is set to title and when theres change it executes the arrow function and passes in the value of the aria-controls=""
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
         </div>
-    );  
-};
-  
-  export default Create;
+        {/* button */}
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
