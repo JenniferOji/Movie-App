cee5e6a4d5053b6d60579ccf4ebc07401e6994bb
JenniferOji <g00413791@atu.ie>
2024-10-18 09:27:55 +0000

    Lab 4 part 3

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,10 +5,18 @@ function Create() {
   //created a state variavle and a method to update it
   //the initial value of title will be an empty string
   const [title, setTitle] = useState('');
+  const [Year, setYear] = useState('');
+  const [Poster, setPoster] = useState('');
 
+
+  //handles what happens to what been submitted
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(title);
+    console.log(Year);
+    console.log(Poster);
+
+
   }
 
   return (
@@ -26,6 +34,41 @@ function Create() {
           />
         </div>
         {/* button */}
+        {/* <input type="submit" value="Add Movie" /> */}
+        <div>
+            <label></label>
+        </div>
+        
+        <div>
+        <label>Add Movie Year: </label>
+          <input type="text"
+            //styling from bootstrap
+            className="form-control"
+            // value is set to title and when theres change it executes the arrow function and passes in the value of the aria-controls=""
+            value={Year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+
+        <div>
+            <label></label>
+        </div>
+        
+        <div>
+        <label>Add Movie Poster: </label>
+          <input type="text"
+            //styling from bootstrap
+            className="form-control"
+            // value is set to title and when theres change it executes the arrow function and passes in the value of the aria-controls=""
+            value={Poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+
+        <div>
+            <label></label>
+        </div>
+           {/* button */}
         <input type="submit" value="Add Movie" />
       </form>
     </div>
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,13 +1,21 @@
 //When the component is called it will be displayed on screen 
 import Movies from "./Movies";
 import { useState, useEffect } from "react";
+//AXIOS
+/*Supports HTTP requests: GET, POST, PUT, DELETE, etc.
+Automatically transforms JSON data.
+Intercepts requests and responses.
+Supports request timeout and cancellation.
+Provides built-in CSRF protection. */
 import axios from "axios";
 
 const Read = () => {
   //manages data in the class
+  //useState returns the current state and the function to update it 
   const [movies,setMovies] = useState([]);
   
       //talking with an external system
+      //lets you synchronize a component with the browser's lifecycle events
       useEffect(
         ()=>{
           //runs on the background not causing the app to hang 
